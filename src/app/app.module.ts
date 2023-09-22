61474525ce41546e6df59daf9cbd4e0141ca2264
Wevioz <wevioz@mail.ru>
2023-09-22 10:18:22 +0300

    main dome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import {MainModule} from "./main/main.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent
+    AppComponent
   ],
   imports: [
+    MainModule,
     BrowserModule,
     AppRoutingModule
   ],
